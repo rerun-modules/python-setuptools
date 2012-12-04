31fb8ffd789fb3617da6ad0a0806414b53f8e797
connaryscott <connaryscott@gmail.com>
2012-12-04 14:30:11 -0800

    change param name format to package-format, release to package-release add package-name to support managing the name of the produced package
    Fixes #2

--- tests/build-1-test.sh
+++ tests/build-1-test.sh
@@ -24,7 +24,27 @@ it_succeeds_with_Pygments() {
 
 it_succeeds_with_PygmentsAndRelease() {
    tmpDir=$(mktemp -d)
-   rerun python-setuptools:build --version 1.5 --release 5 --directory "${tmpDir}" --name Pygments 
+   rerun python-setuptools:build --version 1.5 --package-release 5 --directory "${tmpDir}" --name Pygments 
    rpm -qp "${tmpDir}/Pygments-1.5-5.noarch.rpm"
    rm -rf "${tmpDir}"
+}
+
+it_succeeds_with_NewPackageNameAndRelease() {
+   tmpDir=$(mktemp -d)
+   rerun python-setuptools:build --version 1.5 --package-name MyPygments --package-release 5 --directory "${tmpDir}" --name Pygments 
+
+   if [ ! -f ${tmpDir}/MyPygments-1.5-5.noarch.rpm ]
+   then
+      echo "expected package filename \"${tmpDir}/MyPygments-1.5-5.noarch.rpm\" was not generated" 1>&2
+      return 1
+   fi
+
+   queryName="$(rpm -qp "${tmpDir}/MyPygments-1.5-5.noarch.rpm" --qf '%{NAME}')"
+   if [ "${queryName}" != MyPygments ]
+   then
+      echo "queried package name ${queryName} does not match expected name:  MyPygments" 1>&2
+      return 1
+   fi
+
+   return 0
 }
