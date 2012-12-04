2d1a99a78813c17ed4a1c26b075cd85bda51e1b6
connaryscott <connaryscott@gmail.com>
2012-12-04 15:11:05 -0800

    add new tests

--- tests/build-1-test.sh
+++ tests/build-1-test.sh
@@ -26,33 +26,41 @@ it_succeeds_with_PygmentsAndPackager() {
    tmpDir=$(mktemp -d)
    rerun python-setuptools:build --version 1.5 --directory "${tmpDir}" --name Pygments --packager "a sonbitch packager"
    rpm -qp "${tmpDir}/Pygments-1.5-1.noarch.rpm"
-   echo rm -rf "${tmpDir}"
+
+   queryPackager="$(rpm -qp "${tmpDir}/Pygments-1.5-1.noarch.rpm" --qf '%{PACKAGER}')"
+   if [ "${queryPackager}" != "a sonbitch packager" ]
+   then
+      echo "queried package name ${queryPackager} does not match expected name:  a sonbitch packager" 1>&2
+      return 1
+   fi
+
+   rm -rf "${tmpDir}"
 }
-#
-#it_succeeds_with_PygmentsAndRelease() {
-#   tmpDir=$(mktemp -d)
-#   rerun python-setuptools:build --version 1.5 --package-release 5 --directory "${tmpDir}" --name Pygments 
-#   rpm -qp "${tmpDir}/Pygments-1.5-5.noarch.rpm"
-#   rm -rf "${tmpDir}"
-#}
-#
-#it_succeeds_with_NewPackageNameAndRelease() {
-#   tmpDir=$(mktemp -d)
-#   rerun python-setuptools:build --version 1.5 --package-name MyPygments --package-release 5 --directory "${tmpDir}" --name Pygments 
-#
-#   if [ ! -f ${tmpDir}/MyPygments-1.5-5.noarch.rpm ]
-#   then
-#      echo "expected package filename \"${tmpDir}/MyPygments-1.5-5.noarch.rpm\" was not generated" 1>&2
-#      return 1
-#   fi
-#
-#   queryName="$(rpm -qp "${tmpDir}/MyPygments-1.5-5.noarch.rpm" --qf '%{NAME}')"
-#   if [ "${queryName}" != MyPygments ]
-#   then
-#      echo "queried package name ${queryName} does not match expected name:  MyPygments" 1>&2
-#      return 1
-#   fi
-#
-#   return 0
-#}
-#
+
+it_succeeds_with_PygmentsAndRelease() {
+   tmpDir=$(mktemp -d)
+   rerun python-setuptools:build --version 1.5 --package-release 5 --directory "${tmpDir}" --name Pygments 
+   rpm -qp "${tmpDir}/Pygments-1.5-5.noarch.rpm"
+   rm -rf "${tmpDir}"
+}
+
+it_succeeds_with_NewPackageNameAndRelease() {
+   tmpDir=$(mktemp -d)
+   rerun python-setuptools:build --version 1.5 --package-name MyPygments --package-release 5 --directory "${tmpDir}" --name Pygments 
+
+   if [ ! -f ${tmpDir}/MyPygments-1.5-5.noarch.rpm ]
+   then
+      echo "expected package filename \"${tmpDir}/MyPygments-1.5-5.noarch.rpm\" was not generated" 1>&2
+      return 1
+   fi
+
+   queryName="$(rpm -qp "${tmpDir}/MyPygments-1.5-5.noarch.rpm" --qf '%{NAME}')"
+   if [ "${queryName}" != MyPygments ]
+   then
+      echo "queried package name ${queryName} does not match expected name:  MyPygments" 1>&2
+      return 1
+   fi
+
+   return 0
+}
+
